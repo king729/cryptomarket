764d78708b5992ea1c2d51fb315ceaf87dadb98e
king729 <87287864@qq.com>
2018-02-08 16:52:16 +0800

    debug

--- dags/market_dag.py
+++ dags/market_dag.py
@@ -12,7 +12,7 @@ default_args = {
 
 dag = DAG('miniute_process', default_args=default_args, schedule_interval=timedelta(minutes=1))
 
-templated_command = "python cryptomarket.py"
+templated_command = 'python ../project/cryptomarket.py'
 
 
 task = BashOperator(
