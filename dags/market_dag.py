59fdf701449134cff2ef5573933792a6f29f50c7
king729 <87287864@qq.com>
2018-02-08 16:06:36 +0800

    debug

---
+++ dags/market_dag.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators import BashOperator
+from datetime import datetime, timedelta
+import ConfigParser
+
+default_args = {
+    'owner': 'king',
+    'depends_on_past': False,
+    'start_date': datetime.today() - timedelta(days=1),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=15),
+}
+
+dag = DAG('miniute_process', default_args=default_args, schedule_interval=timedelta(minutes=1))
+
+templated_command = "python cryptomarket.py"\
+    + " --start_date {{ ds }}"
+
+
+task = BashOperator(
+    task_id='process_log',
+    bash_command=templated_command,
+    dag=dag
+)
